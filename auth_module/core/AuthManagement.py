8f671a45eac079786e1b3710e319f13a6f8cce3b
Immanuel01 <immanuel01@ui.ac.id>
2023-03-02 17:59:28 +0700

    [RED] Implement AuthManagement.py
    Signed-off-by: Immanuel01 <immanuel01@ui.ac.id>

--- auth_module/core/AuthManagement.py
+++ auth_module/core/AuthManagement.py
@@ -1,19 +1,45 @@
+from cachetools import TTLCache
+
 from auth_module.core.RandomFactory import RandomFactory
+from auth_module.exceptions.InvalidTokenException import InvalidTokenException
 
 
 # singleton
 class AuthManagement:
+    instance = None
+
     @staticmethod
     def get_instance():
-        raise NotImplementedError()
+        if AuthManagement.instance is None:
+            AuthManagement.instance = AuthManagement()
+        return AuthManagement.instance
 
-    _random_factory = RandomFactory()
+    def __init__(self):
+        self._random_factory = RandomFactory()
+        ONE_HOUR = 60*60
+        ONE_DAY = 24 * ONE_HOUR
+        ONE_MONTH = 31 * ONE_DAY
+        INF = float('inf')
+        self._token_to_user_mapping = TTLCache(ttl=ONE_MONTH, maxsize=INF)
+
 
     def get_user(self, token: str):
-        raise NotImplementedError()
+        if token not in self._token_to_user_mapping:
+            raise InvalidTokenException()
+        return self._token_to_user_mapping[token]
 
     def register_token(self, user):
-        raise NotImplementedError()
+        token = self.__generate_random_token()
+        self._token_to_user_mapping[token] = user
+        return token
+
 
     def __generate_random_token(self) -> str:
-        raise NotImplementedError()
+        while True:
+            random_token = self._random_factory.random_string(16)
+            if random_token not in self._token_to_user_mapping:
+                break
+        return random_token
+
+
+AuthManagement.get_instance()  # initialize
