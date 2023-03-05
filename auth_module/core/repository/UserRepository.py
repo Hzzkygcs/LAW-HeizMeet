b5111ce44e304dbf2eca35b9fed1c2bf3dbf702b
Immanuel01 <immanuel01@ui.ac.id>
2023-03-06 03:35:51 +0700

    [GREEN] UserRepository implementation
    Signed-off-by: Immanuel01 <immanuel01@ui.ac.id>

--- auth_module/core/repository/UserRepository.py
+++ auth_module/core/repository/UserRepository.py
@@ -6,10 +6,10 @@ from auth_module.models import User
 @inject
 class UserRepository:
     def register_new_user(self, user: User):
-        raise NotImplementedError()
+        user.save()
 
     def find_user_by_email(self, email: str) -> User:
-        raise NotImplementedError()
+        return User.objects.get(email=email)
 
 
 
