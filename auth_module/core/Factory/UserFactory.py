55f94398e9f58502f62156f4146fd42c8a1ad8aa
Immanuel01 <immanuel01@ui.ac.id>
2023-03-06 03:13:28 +0700

    [GREEN] implement UserFactory
    Signed-off-by: Immanuel01 <immanuel01@ui.ac.id>

--- auth_module/core/Factory/UserFactory.py
+++ auth_module/core/Factory/UserFactory.py
@@ -4,9 +4,12 @@ from auth_module.models import User
 
 
 class UserFactory:
-    def __init__(self):
-        raise NotImplementedError()
-
     def create_user(self, email: str, password: Union[bytes, str]):
+        user = User()
+        user.email = email
 
-        raise NotImplementedError()
+        if isinstance(password, str):
+            password = password.encode()
+        user.password = password
+
+        return user
