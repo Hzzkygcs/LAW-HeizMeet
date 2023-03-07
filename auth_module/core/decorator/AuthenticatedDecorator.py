b8257b06480b950829ac88c4ddb80cb87b50cb9b
Immanuel01 <immanuel01@ui.ac.id>
2023-03-08 02:56:29 +0700

    balikin ke yang berhasil
    Signed-off-by: Immanuel01 <immanuel01@ui.ac.id>

--- auth_module/core/decorator/AuthenticatedDecorator.py
+++ auth_module/core/decorator/AuthenticatedDecorator.py
@@ -1,6 +1,5 @@
-import inspect
 import queue
-from inspect import ismethod, getfullargspec, getmembers
+from inspect import ismethod, getfullargspec
 
 from kink import di, inject
 
@@ -9,8 +8,10 @@ from auth_module.exceptions.NotLoggedInException import NotLoggedInException
 
 
 # decorator
-# accepts func(self, req, logged_in_user, *args, **kwargs)
-# returns func(self, req, *args, **kwargs)
+# accepts:  func(self, req, *args, logged_in_user, **kwargs)
+# injects: logged_in_user
+# returns callable: func(self, req, *args, **kwargs)
+
 class AuthenticatedDecorator:
     def __init__(self):
         self.__auth_management = di[AuthManagement]
@@ -23,50 +24,42 @@ class AuthenticatedDecorator:
     def auth_management(self, value: AuthManagement):
         self.__auth_management = value
 
-    def cls(self, cls):
-        return self.class_decorator(cls)
-
     def __call__(self, method):
-        return self.method_decorator(method)
+        args = getfullargspec(method).args
+        is_method = False
+        if len(args) > 1 and ("self" in args[0].lower()):
+            is_method = True
+        return self.__decorate(method, is_method)
 
-    def method_decorator(self, method):
-        method._AuthenticatedDecorator_need_authentication = True
-        return method
+    def __decorate(self, method_or_func, is_method):
+        def wrapper(arg1, *args_list, **kwargs):
+            req = arg1
+            if is_method:
+                req = args_list[0]  # args2
 
-    def class_decorator(self, cls):
-        outter_self = self
-        orig_init = cls.__init__
+            logged_in_user = self.get_user_object(req, wrapper)
+            args_list = args_list + (logged_in_user,)
+            return method_or_func(arg1, *args_list, **kwargs)
+        wrapper.user_mock = queue.Queue()
+        wrapper.auth_management = self.auth_management
+        def restore_auth_management():
+            wrapper.auth_management = self.auth_management
+        wrapper.restore_auth_management = restore_auth_management
+        wrapper.add_user_mock = lambda user_mock: wrapper.user_mock.put(user_mock)
+        def set_user_mock(user_mock):
+            wrapper.user_mock = queue.Queue()
+            wrapper.add_user_mock(user_mock)
+        wrapper.set_user_mock = set_user_mock
 
-        def __init__(self, *args, **kwargs):
-            orig_init(self, *args, **kwargs)
-            self._AuthenticatedDecorator_user_mock = queue.Queue()
-            self._AuthenticatedDecorator_auth_management = outter_self.auth_management
-            self.add_user_mock = lambda mock: self._AuthenticatedDecorator_user_mock.put(mock)
-        cls.__init__ = __init__
+        return wrapper
 
-        for name, method in getmembers(cls):
-            if hasattr(method, "_AuthenticatedDecorator_need_authentication"):
-                def wrap(method):
-                    def new_method(inner_self, req, *args, **kwargs):
-                        logged_in_user = self.get_user_object(req, inner_self)
-                        args = args + (logged_in_user,)
-                        return method(self, req, *args, **kwargs)
-                    return new_method
-                setattr(cls, name, wrap(method))
-        return cls
-
-
-    def get_user_object(self, req, instance):
-        user_mock = instance._AuthenticatedDecorator_user_mock
-        auth_management = instance._AuthenticatedDecorator_auth_management
-
-        if not user_mock.empty():  # for mocking purpose
-            return user_mock.get()
+    def get_user_object(self, req, wrapper):
+        if not wrapper.user_mock.empty():  # for mocking purpose
+            return wrapper.user_mock.get()
         if 'token' not in req.COOKIES:
             raise NotLoggedInException()
         token = req.COOKIES['token']
-        return auth_management.get_user(token)
-
+        return wrapper.auth_management.get_user(token)
 
 
 authenticated = AuthenticatedDecorator()
