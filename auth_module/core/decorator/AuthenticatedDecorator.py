f26a5507ce867d154d1cf301031de5748558e115
Immanuel01 <immanuel01@ui.ac.id>
2023-03-08 01:46:26 +0700

    backup masih error
    Signed-off-by: Immanuel01 <immanuel01@ui.ac.id>

--- auth_module/core/decorator/AuthenticatedDecorator.py
+++ auth_module/core/decorator/AuthenticatedDecorator.py
@@ -1,3 +1,4 @@
+import queue
 from inspect import ismethod, getfullargspec
 
 from kink import di, inject
@@ -21,29 +22,43 @@ class AuthenticatedDecorator:
     def auth_management(self, value: AuthManagement):
         self.__auth_management = value
 
-    def __call__(self, method_or_func):
-        args = getfullargspec(method_or_func).args
-        if len(args) > 1 and args[0] == "self":
-            return self.method(method_or_func)
-        return self.func(method_or_func)
+    def __call__(self, method):
+        args = getfullargspec(method).args
+        is_method = False
+        if len(args) > 1 and ("self" in args[0].lower()):
+            is_method = True
+        return self.__decorate(method, is_method)
 
-    def method(self, method):
-        def method_wrapper(their_self, req, *args, **kwargs):
-            logged_in_user = self.get_user_object(req)
-            return method(their_self, req, logged_in_user, *args, **kwargs)
-        return method_wrapper
+    def __decorate(self, method_or_func, is_method):
 
-    def func(self, func, *args, **kwargs):
-        def func_wrapper(req, *args, **kwargs):
-            logged_in_user = self.get_user_object(req)
-            return func(req, logged_in_user, *args, **kwargs)
-        return func_wrapper
+        class wrapper:
+            def __init__(self_2nd):
+                self_2nd._user_mock = queue.Queue()
+                self_2nd.auth_management = self.auth_management
 
-    def get_user_object(self, req):
-        if 'token' not in req.COOKIES:
-            raise NotLoggedInException()
-        token = req.COOKIES['token']
-        return self.auth_management.get_user(token)
+            def __call__(self_2nd, arg1, *args_list, **kwargs):
+                req = arg1
+                if is_method:
+                    req = args_list[0]  # args2
+
+                logged_in_user = self_2nd.get_user_object(req)
+                args_list = args_list + (logged_in_user,)
+                return method_or_func(arg1, *args_list, **kwargs)
+
+            def restore_auth_management(self_2nd):
+                self_2nd.auth_management = self.auth_management
+
+            def __addi__(self_2nd, user_mock):  # to add mock
+                self_2nd._user_mock.put(user_mock)
+
+            def get_user_object(self_2nd, req):
+                if not self_2nd._user_mock.empty():  # for mocking purpose
+                    return self_2nd._user_mock.get()
+                if 'token' not in req.COOKIES:
+                    raise NotLoggedInException()
+                token = req.COOKIES['token']
+                return self_2nd.auth_management.get_user(token)
+        return wrapper()
 
 
 authenticated = AuthenticatedDecorator()
