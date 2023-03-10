8daeacf5f9e9add36422ae32b0a315b9533210c1
Immanuel01 <immanuel01@ui.ac.id>
2023-03-11 00:47:31 +0700

    gatau
    Signed-off-by: Immanuel01 <immanuel01@ui.ac.id>

--- schedule/static/event-create/js.js
+++ schedule/static/event-create/js.js
@@ -1,48 +1,72 @@
 let schedules = [];
 
 
-const START_DATE_EL = "#start-date";
-const END_DATE_EL = "#end-date";
+const DATE_EL = "#schedule-date";
+const START_TIME_EL = "#start-time";
+const END_TIME_EL = "#end-time";
 
 
-function setToToday(el, addMiliSecond){
-    addMiliSecond += 7 * 60 * 60 * 1000;  // UTF+7
 
-    let date = new Date();
-    date = new Date(date.getTime() + addMiliSecond);
-
-    $(el).val(date.toJSON().slice(0,19));
-}
 
 function showModal(){
-    if ($(START_DATE_EL).val() === '')
-        setToToday($(START_DATE_EL), 0);
-    if ($(END_DATE_EL).val() === '')
-        setToToday($(END_DATE_EL), 30*60*1000);
     $('#authentication-modal').css('display', 'grid');
+    if ($(DATE_EL).val() === '')
+        setToToday($(DATE_EL), 0);
+    if ($(START_TIME_EL).val() === '')
+        setToThisTime($(START_TIME_EL), 30*60*1000);
+    if ($(END_TIME_EL).val() === '')
+        setToThisTime($(END_TIME_EL), 60*60*1000);
 }
+
+function setToToday(el, addMiliSecond){
+    console.log("today")
+    addMiliSecond += 7 * 60 * 60 * 1000;  // UTF+7
+
+    let dateObj = new Date();
+    dateObj = new Date(dateObj.getTime() + addMiliSecond);
+    const date = dateObj.getDate();
+    const month = dateObj.getMonth();
+    const year = dateObj.getFullYear();
+
+    const text = `${date}/${month}/${year}`;
+    console.log(text);
+    $(el).val(text);
+}
+function setToThisTime(el, addMiliSecond){
+    addMiliSecond += 7 * 60 * 60 * 1000;  // UTF+7
+
+    let dateObj = new Date();
+    dateObj = new Date(dateObj.getTime() + addMiliSecond);
+    const hour = dateObj.getHours();
+    const minutes = dateObj.getMinutes();
+
+    $(el).val(`${hour}:${minutes}`);
+}
+
 
 function hideModal(){
     $('#authentication-modal').css('display', 'none');
 }
 
-function submitModal(){
-    let start = $(START_DATE_EL).val();
-    let end = $(END_DATE_EL).val();
-    start = new Date(start);
-    end = new Date(end);
+function getScheduleObjFromModalInput(){
+    let date = getDateObjFromDatePicker()
+    let start = getDataFromTimePicker($(START_TIME_EL));
+    let end = getDataFromTimePicker($(END_TIME_EL));
+
     if (start > end){
         alert("Start time cannot be greater than the end time");
         return;
     }
+    return new Schedule(date, start, end);
+}
 
+function submitModal(){
     let schedulesCopy = schedules.slice();
 
-    schedulesCopy.push({
-        start: start,
-        end: end,
-    });
-    schedulesCopy = schedulesCopy.sort((a, b) => a.start - b.start);
+    const schedule = getScheduleObjFromModalInput();
+    schedulesCopy.push(schedule);
+    schedulesCopy = schedulesCopy.sort();
+
     if (!noOverlappingSchedule(schedulesCopy)) {
         console.log("Overlap");
         alert("This schedule overlaps another schedule");
@@ -52,6 +76,10 @@ function submitModal(){
     schedules = schedulesCopy;
     hideModal();
 }
+
+
+
+
 
 function noOverlappingSchedule(schedules){
     schedules = schedules.sort((a, b) => a.start - b.start);
